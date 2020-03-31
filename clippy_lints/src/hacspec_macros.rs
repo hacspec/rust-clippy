5bbd2b7469d9ef5788a282b71e4a05b344f7e829
Florian Groult <florian.groult@inria.fr>
2020-03-31 11:31:00 +0200

    whitelisted macros and added types declared to field struct

--- clippy_lints/src/hacspec_macros.rs
+++ clippy_lints/src/hacspec_macros.rs
@@ -1,9 +1,10 @@
 use crate::utils::span_lint;
 use rustc_lint::{EarlyLintPass, EarlyContext};
-use rustc_session::{declare_lint_pass, declare_tool_lint};
-// use rustc_span::Span;
+use rustc_session::{impl_lint_pass, declare_tool_lint};
 use rustc_ast::{
     ast::{MacCall, PathSegment},
+    tokenstream::TokenTree,
+    token::{Token, TokenKind},
 };
 
 declare_clippy_lint! {
@@ -23,9 +24,91 @@ declare_clippy_lint! {
     "default lint description"
 }
 
-declare_lint_pass!(HacspecMacros => [HACSPEC_MACROS]);
+#[derive(Default)]
+pub struct HacspecMacros {
+    added_macros: Vec<Vec<String>>,
+}
+
+impl_lint_pass!(HacspecMacros => [HACSPEC_MACROS]);
+
+
+
+const ALLOWED_MACRO_TYPE_DECL: &[&[&str]] = &[
+    &["array"],
+    &["bytes"],
+    &["public_bytes"],
+    &["public_array"],
+    &["poly"],
+    &["field_integer"],
+    //soon to be replaced by
+    // nat_mod
+];
+
+const ALLOWED_MACROS_MISC : &[&[&str]] = &[
+    &["concat"],
+    &["secret_array"],
+    &["secret_bytes"],
+    &["assert"],
+    &["assert_bytes_eq"],
+    &["assert_eq"],
+];
+
+fn allowed_path(authorized_macros: &[&[&str]], queried_use: &[PathSegment]) -> bool {
+    authorized_macros.iter().any(|&allowed_use| {
+        allowed_use
+            .iter()
+            .zip(queried_use.iter())
+            .filter(|(&allowed_segment, queried_segment)| *allowed_segment == *queried_segment.ident.name.as_str())
+            .count()
+            == allowed_use.len()
+    })
+}
+
+// not really nice for code reuse
+impl HacspecMacros {
+    fn check_added_macro(&self, queried_use: &[PathSegment]) -> bool {
+        self.added_macros.iter().any(|allowed_use| {
+            allowed_use
+                .iter()
+                .zip(queried_use.iter())
+                .filter(|(allowed_segment, queried_segment)| *allowed_segment.as_str() == *queried_segment.ident.name.as_str())
+                .count()
+                == allowed_use.len()
+        })
+    }
+}
 
 impl EarlyLintPass for HacspecMacros {
-    fn check_mac(&mut self, _cx: &EarlyContext<'_>, mac: &MacCall) {
+    fn check_mac(&mut self, cx: &EarlyContext<'_>, mac: &MacCall) {
+        if allowed_path(ALLOWED_MACRO_TYPE_DECL, &mac.path.segments) {
+            let mut flag = false;
+            &mac.args.inner_tokens().map_enumerated(
+                |i, tk_tr| {
+                    if i == 0 {
+                        if let TokenTree::Token( Token { kind:TokenKind::Ident(name, _b), span: _ }) = tk_tr {
+                            name.with(
+                                |s| {   // if self.added_macros.contains(String::from(s)) {
+                                        //     span_lint(cx, HACSPEC_MACROS, mac.span(), &format!("Already declared type {}", type_decl)) }
+                                        // shouldn't be possible, macro expansion would cause an error, identifier already used or something like this
+                                        self.added_macros.push(vec![String::from(s)]);
+                                        flag = true;
+                                    }
+                            );
+                        }
+                    };  tk_tr }
+            );
+            if flag {
+                return;
+            } else { span_lint(cx, HACSPEC_MACROS, mac.span(), "FORBIDDEN MACRO") }
+        }
+        if allowed_path(ALLOWED_MACROS_MISC, &mac.path.segments)
+        ||  self.check_added_macro(&mac.path.segments) { return; }
+
+        span_lint(
+            cx,
+            HACSPEC_MACROS,
+            mac.span(),
+            "FORBIDDEN MACRO",
+        )
     }
 }
--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -339,7 +339,7 @@ mod reexport {
 /// Used in `./src/driver.rs`.
 pub fn register_pre_expansion_lints(store: &mut rustc_lint::LintStore, conf: &Conf) {
     store.register_pre_expansion_pass(|| box write::Write::default());
-    store.register_pre_expansion_pass(|| box hacspec_macros::HacspecMacros);
+    store.register_pre_expansion_pass(|| box hacspec_macros::HacspecMacros::default());
     store.register_pre_expansion_pass(|| box redundant_field_names::RedundantFieldNames);
     let single_char_binding_names_threshold = conf.single_char_binding_names_threshold;
     store.register_pre_expansion_pass(move || box non_expressive_names::NonExpressiveNames {
